878150122e07ba53b9d0b068fff218ba2243c8dd
Sam Williams <samuel.williams@gmail.com>
2017-02-11 14:00:00 -0500

    hex character set is public

--- ModelKit/Fields/CharacterSet+extensions.swift
+++ ModelKit/Fields/CharacterSet+extensions.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 public extension CharacterSet {
-    static var uppercaseHexadecimal = CharacterSet(charactersIn: "ABCDEF0123456789")
-    static var lowercaseHexadecimal = CharacterSet(charactersIn: "abcdef0123456789")
+    public static var uppercaseHexadecimal = CharacterSet(charactersIn: "ABCDEF0123456789")
+    public static var lowercaseHexadecimal = CharacterSet(charactersIn: "abcdef0123456789")
 }
