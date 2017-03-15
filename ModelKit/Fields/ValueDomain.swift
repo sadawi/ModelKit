c3ce5fbebc6331b0b7449317dcd78f417a8c9033
Sam Williams <samuel.williams@gmail.com>
2017-03-15 00:25:31 -0400

    init for domains

--- ModelKit/Fields/ValueDomain.swift
+++ ModelKit/Fields/ValueDomain.swift
@@ -15,30 +15,39 @@ open class ValueDomain<T: Equatable> {
 }
 
 open class ContinuousValueDomain<T: Comparable>: ValueDomain<T> {
-    open var minimum: T?
-    open var maximum: T?
+    open var lowerBound: T?
+    open var upperBound: T?
 
     open override func contains(_ value: T) -> Bool {
-        if let min = self.minimum, value < min {
+        if let min = self.lowerBound, value < min {
             return false
         }
         
-        if let max = self.maximum, value < max {
+        if let max = self.upperBound, value < max {
             return false
         }
         
         return true
     }
+    
+    public required init(lowerBound: T? = nil, upperBound: T? = nil) {
+        self.lowerBound = lowerBound
+        self.upperBound = upperBound
+    }
 }
 
-open class DiscreteValueDomain<T: Equatable>: ValueDomain<T> {
+open class DiscreteValueDomain<T: Equatable>: ValueDomain<T>, ExpressibleByArrayLiteral {
     open var values: [T] = []
     
-    init(_ values: [T]) {
+    public required init(_ values: [T]) {
         self.values = values
     }
     
     open override func contains(_ value: T) -> Bool {
         return self.values.contains(value)
+    }
+
+    public required convenience init(arrayLiteral elements: T...) {
+        self.init(elements)
     }
 }
